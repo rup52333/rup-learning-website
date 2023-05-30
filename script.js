c7937983ef0bc3c8d6f12d9e575bf0bd05ead62c
Rupkumar Sarkar <109466063+rup52333@users.noreply.github.com>
2023-05-30 12:25:52 +0530

    2nd

---
+++ script.js
@@ -0,0 +1,6 @@
+$(document).ready(function() {
+    $('#myCarousel').carousel({
+      interval: 3000, // Set the interval between slides (in milliseconds)
+      wrap: true // Enable looping of the carousel
+    });
+  });
